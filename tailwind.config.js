31f299ca6acdc735ee50e34568d8e05ecc436d7c
mateeusgl <magolopes20@gmail.com>
2022-05-03 23:15:08 -0300

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: '#8257E5'
+        },
+        brandHover: {
+          400: '#996DFF'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
